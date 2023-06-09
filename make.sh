1c3877a1bbeb2127192793ca6842b2908710d7b7
David Baucum <david@baucum.me>
2023-06-09 19:47:48 -0400

    Update make to create file in a build directory

--- make.sh
+++ make.sh
@@ -2,5 +2,6 @@
 
 command -v zip >/dev/null 2>&1 || { echo >&2 "The zip utility was not found on this system."; exit 1; }
 
-zip -X ../quest_for_fire.epub mimetype
-zip -rg ../quest_for_fire.epub META-INF OPS
+mkdir -p build
+zip -X build/quest_for_fire.epub mimetype
+zip -rg build/quest_for_fire.epub META-INF OPS
