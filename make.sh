ee1bc9c56944f510bb8507b1588c0c9b3634c3fd
David Baucum <david@baucum.me>
2023-06-12 00:45:50 -0400

    Update make script to name book war_for_fire.epub

--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@
 command -v zip >/dev/null 2>&1 || { echo >&2 "The zip utility was not found on this system."; exit 1; }
 
 mkdir -p build
-zip -X build/quest_for_fire.epub mimetype
-zip -rg build/quest_for_fire.epub META-INF OPS
+zip -X build/war_for_fire.epub mimetype
+zip -rg build/war_for_fire.epub META-INF OPS
